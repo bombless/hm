47c3eb52efe30c20971767585f6fe26dafa48c98
York Xiang <bombless@126.com>
2017-05-16 13:12:48 +0800

    Failed: seems like wrong route

--- src/lib.rs
+++ src/lib.rs
@@ -109,16 +109,78 @@ trait Visitor {
     fn visit(&mut self, e: Expr);
 }
 
+impl Visitor {
+    fn top_down<T: Visitor>(v: &mut T, e: Expr) {
+        handle(v, e);
+        fn handle(v: &mut T, e: Expr) {
+            use Expr::*;
+            v.visit(e.clone());
+            match e {
+                App(_, ref e1, ref e2) => {
+                    handle(v, Expr::clone(e1));
+                    handle(v, Expr::clone(e2));
+                },
+                Abs(_, _, ref e) => handle(v, Expr::clone(e)),
+                Let(_, _, ref e1, ref e2) => {
+                    handle(v, Expr::clone(e1));
+                    handle(v, Expr::clone(e2));
+                },
+                _ => ()
+            }
+        }
+    }
+    fn down_top<T: Visitor>(v: &mut T, e: Expr) {
+        handle(v, e);
+        fn handle(v: &mut T, e: Expr) {
+            use Expr::*;
+            match &e {
+                &App(_, ref e1, ref e2) => {
+                    handle(v, Expr::clone(e1));
+                    handle(v, Expr::clone(e2));
+                },
+                &Abs(_, _, ref e) => handle(v, Expr::clone(e)),
+                &Let(_, _, ref e1, ref e2) => {
+                    handle(v, Expr::clone(e1));
+                    handle(v, Expr::clone(e2));
+                },
+                _ => ()
+            }
+            v.visit(e);
+        }
+    }
+}
 
+impl Visitor for KnowledgeBase {
+    fn visit(&mut self, e: Expr) {
+        if let Some((eid, tid)) = resolve(e, self) {
+            self.knowledge.insert(eid, tid);
+        }
+    }
+}
 
 
 fn resolve(e: Expr, k: &KnowledgeBase) -> Option<(ExprIdx, TypeIdx)> {
     use Expr::*;
-    match e {
+    let e_match = e.clone();
+    match e_match {
         Var(eid, _) => match k.get(eid).map(t => (eid, t)),
         App(eid, e1, e2) => match (k.get(&e1.id()), k.get(&e2.id())) {
             (Some(tid1), Some(tid2)) => Some((eid, k.apply(tid1, tid2))),
             _ => None,
+        },
+        Abs(eid, (arg, _), expr) => {
+            if k.get(&expr.id()).is_none() {
+                let idx_bound = <Into<u32>>::into(*k.next_idx.get());
+                for i in 0 .. idx_bound {
+                    let type_idx = i.into();
+                    let mut knowledge = k.clone();
+                    knowledge.knowledge.insert(eid, type_idx);
+                    Visitor::top_down(&mut knowledge, e);
+                    Visitor::down_top(&mut knowledge, e);
+                    Visitor::top_down(&mut knowledge, e);
+                    Visitor::down_top(&mut knowledge, e);
+                }
+            }
         },
         Abs(eid, (arg, _), expr) => Some((eid, k.lambda(k.get(&arg), k.get(&expr)))),
         Let(eid, (var, _), val, env) => 
